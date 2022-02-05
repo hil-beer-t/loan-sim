a95245d88cbb0973fd1ae65b1cc8e97a900f86ef
hil-beer-t <hil.beer.t.git@gmail.com>
2022-02-05 02:00:38 -0300

    [BK] model: client
    Create a client model without ORM.

---
+++ backend/src/main/java/com/hilbert/loansimapi/models/Client.java
@@ -0,0 +1,96 @@
+package com.hilbert.loansimapi.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "client")
+public class Client {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private Double income;
+
+    @Column(unique = true)
+    private String cpf;
+
+    @Column(unique = true)
+    private String rg;
+
+    private String address;
+
+    private String password;
+
+    public Client() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Double getIncome() {
+        return income;
+    }
+
+    public void setIncome(Double income) {
+        this.income = income;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
