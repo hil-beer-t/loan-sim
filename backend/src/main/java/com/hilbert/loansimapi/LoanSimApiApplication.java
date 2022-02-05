a836ab68cfc084fce5e155545915c060cb023f98
hil-beer-t <hil.beer.t.git@gmail.com>
2022-02-05 02:24:47 -0300

    [BK] model: month
    Create an enum month model.
    Represents the 12 months in gregorian calendar, JAN-DEC, 0-11.

--- backend/src/main/java/com/hilbert/loansimapi/LoanSimApiApplication.java
+++ backend/src/main/java/com/hilbert/loansimapi/LoanSimApiApplication.java
@@ -1,13 +1,19 @@
 package com.hilbert.loansimapi;
 
+import com.hilbert.loansimapi.models.Month;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LoanSimApiApplication {
+public class LoanSimApiApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LoanSimApiApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+
+	}
 }
---
+++ backend/src/main/java/com/hilbert/loansimapi/models/Month.java
@@ -0,0 +1,64 @@
+package com.hilbert.loansimapi.models;
+
+public enum Month {
+    /**
+     * The singleton instance for the month of January with 31 days.
+     * This has the numeric value of {@code 0}.
+     */
+    JANUARY,
+    /**
+     * The singleton instance for the month of February with 28 days, or 29 in a leap year.
+     * This has the numeric value of {@code 1}.
+     */
+    FEBRUARY,
+    /**
+     * The singleton instance for the month of March with 31 days.
+     * This has the numeric value of {@code 2}.
+     */
+    MARCH,
+    /**
+     * The singleton instance for the month of April with 30 days.
+     * This has the numeric value of {@code 3}.
+     */
+    APRIL,
+    /**
+     * The singleton instance for the month of May with 31 days.
+     * This has the numeric value of {@code 4}.
+     */
+    MAY,
+    /**
+     * The singleton instance for the month of June with 30 days.
+     * This has the numeric value of {@code 5}.
+     */
+    JUNE,
+    /**
+     * The singleton instance for the month of July with 31 days.
+     * This has the numeric value of {@code 6}.
+     */
+    JULY,
+    /**
+     * The singleton instance for the month of August with 31 days.
+     * This has the numeric value of {@code 7}.
+     */
+    AUGUST,
+    /**
+     * The singleton instance for the month of September with 30 days.
+     * This has the numeric value of {@code 8}.
+     */
+    SEPTEMBER,
+    /**
+     * The singleton instance for the month of October with 31 days.
+     * This has the numeric value of {@code 9}.
+     */
+    OCTOBER,
+    /**
+     * The singleton instance for the month of November with 30 days.
+     * This has the numeric value of {@code 10}.
+     */
+    NOVEMBER,
+    /**
+     * The singleton instance for the month of December with 31 days.
+     * This has the numeric value of {@code 11}.
+     */
+    DECEMBER;
+}
