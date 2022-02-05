3e47ad29b4a0c5fc7555a821a0962d0099fc1e18
hil-beer-t <hil.beer.t.git@gmail.com>
2022-02-05 01:29:12 -0300

    [BK] Initial commit
    Basic structure and profiles.
    - Config H2 database, 'application-test.properties'
    - Config Postgres, 'application-dev.properties'
    
    [BK] - Backend
    [FR] - Frontend

---
+++ backend/src/main/java/com/hilbert/loansimapi/LoanSimApiApplication.java
@@ -0,0 +1,13 @@
+package com.hilbert.loansimapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanSimApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanSimApiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/hilbert/loansimapi/LoanSimApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hilbert.loansimapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanSimApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
