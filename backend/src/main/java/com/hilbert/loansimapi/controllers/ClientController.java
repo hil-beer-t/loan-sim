761829888b8e70224d456fa96590fd5e8970546b
hil-beer-t <hil.beer.t.git@gmail.com>
2022-02-05 04:32:38 -0300

    [BK] feat: add /clients endpoint [POST]
    Create one client. Method: POST. Return the client created.

--- backend/src/main/java/com/hilbert/loansimapi/controllers/ClientController.java
+++ backend/src/main/java/com/hilbert/loansimapi/controllers/ClientController.java
@@ -4,10 +4,10 @@ import com.hilbert.loansimapi.models.Client;
 import com.hilbert.loansimapi.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,6 +22,22 @@ public class ClientController {
     public ResponseEntity<List<Client>> getAllClients(){
         List<Client> clients = new ArrayList<>(clientRepository.findAll());
         return ResponseEntity.ok(clients);
+    }
+
+    @PostMapping
+    public ResponseEntity<Client> insertOneClient(@RequestBody Client clientInBody){
+
+        Client client = clientRepository.findByEmail(clientInBody.getEmail());
+
+        if ( client != null){
+            return ResponseEntity.unprocessableEntity().build();
+        }
+
+        // TODO: ENCODE PASSWORD -> clientInBody.setPassword(passwordEncoder.encode(clientInBody.getPassword()));
+        clientInBody = clientRepository.save(clientInBody);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+                .buildAndExpand(clientInBody.getId()).toUri();
+        return ResponseEntity.created(uri).body(clientInBody);
     }
 
 }
--- backend/src/main/java/com/hilbert/loansimapi/repositories/ClientRepository.java
+++ backend/src/main/java/com/hilbert/loansimapi/repositories/ClientRepository.java
@@ -4,4 +4,5 @@ import com.hilbert.loansimapi.models.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ClientRepository extends JpaRepository<Client, Long> {
+    Client findByEmail(String email);
 }
